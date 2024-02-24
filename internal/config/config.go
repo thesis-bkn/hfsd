6768fd2c34273eba6317c7e54abe3470d108decb
khangkontum <khangkontum@gmail.com>
2024-02-24 10:40:25 +0700

    feat: add minio to docker-compose

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,8 +7,7 @@ import (
 )
 
 type Config struct {
-	Port int    `env:"PORT"         envDefault:"2002"`
-	URI  string `env:"DATABASE_URL"`
+	Port int `env:"PORT" envDefault:"2002"`
 
 	Authenticate struct {
 		JwtSecret string `env:"JWT_SECRET"`
